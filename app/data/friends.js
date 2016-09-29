518a63592b948cbbe9821350511a58a71227f007
illynabruin92 <alvaivette@g.ucla.edu>
2016-09-28 22:54:39 -0700

    updated friends.js objects

---
+++ app/data/friends.js
@@ -0,0 +1,346 @@
+/* Girl names: Olivia, Sophia, Ava, Isabella, Mia, Abigail, Charlotte, Harper, Amelia, Isla, Aria, Aurora, Arabella, Sophie, Grace, Ella, Lucy, Aoife, Lily, Hannah, Chloe, Anna, Ellie, Holly, Ruby, Saoirse, Lauren, Caoimhe, Leah, Ciara, Molly, Amy, Jessica, Zoe, Niamh, Cara, Freya, Roisin, Erin, Sadie, Julia, Eva, Clodagh, Sadhbh, Kayla, Millie, Eve, Abbie, Laura, Layla, Faye, Sienna
+
+Boy names: Noah, Liam, Mason, Jacob, William, Ethan, James, Alexander, Michael, Benjamin, Ezra, Asher, Declan, Atticus, Oliver, Milo, Levi, Silas, Jack, Jasper, Conor, Sean. Adam, Luke, Charlie, Dylan, Oisin, Liam, Cian, Patrick, Cillian, Darragh, Fionn, Evan, Finn, Rian, Callum, Eoin, Rio, Sebastian, Eoghan, Alfie, Shane, Senan, Ciaran, Ronan, Rory, Scott, Logan, Kai, Dara, Ollie, Niall, Luca
+
+*/
+
+
+{
+  "name":"Olivia",
+  "photo":"https://i.ytimg.com/vi/yevSZtUmjes/maxresdefault.jpg",
+  "scores":[
+     5,
+     1,
+     4,
+     4,
+     5,
+     1,
+     2,
+     5,
+     4,
+     1
+   ]
+}
+
+{
+  "name":"Sophia",
+  "photo":"http://femaleloophole.org/wp-content/uploads/2011/07/female-psychology-loophole.jpg",
+  "scores":[
+     3,
+     1,
+     2,
+     5,
+     1,
+     4,
+     3,
+     1,
+     3,
+     5
+   ]
+}
+
+{
+  "name":"Ava",
+  "photo":"http://kingofwallpapers.com/female/female-022.jpg",
+  "scores":[
+     5,
+     2,
+     3,
+     5,
+     5,
+     3,
+     2,
+     1,
+     2,
+     1
+   ]
+}
+
+{
+  "name":"Isabella",
+  "photo":"http://fashion.blogorganization.com/wp-includes/images/322female-celebrity-long-hair-photos-04.jpg",
+  "scores":[
+     5,
+     1,
+     3,
+     5,
+     2,
+     3,
+     4,
+     5,
+     1,
+     5
+   ]
+}
+
+{
+  "name":"Mia",
+  "photo":"https://s-media-cache-ak0.pinimg.com/originals/3d/2f/45/3d2f45f6f3ef6164fcf793aa9cce1968.jpg",
+  "scores":[
+     1,
+     5,
+     4,
+     4,
+     2,
+     3,
+     4,
+     5,
+     1,
+     5
+   ]
+}
+
+{
+  "name":"Abigail",
+  "photo":"https://i.ytimg.com/vi/L9ybIqXeRNA/hqdefault.jpg",
+  "scores":[
+     5,
+     1,
+     4,
+     4,
+     5,
+     1,
+     2,
+     5,
+     4,
+     1
+   ]
+}
+
+{
+  "name":"Charlotte",
+  "photo":"https://assets.entrepreneur.com/content/16x9/822/1387219460-leadership-lessons-young-female-entrepreneurs-andi-atteberry-blingsting.jpg",
+  "scores":[
+     2,
+     3,
+     4,
+     4,
+     5,
+     2,
+     3,
+     5,
+     2,
+     3
+   ]
+}
+
+{
+  "name":"Harper",
+  "photo":"https://sosv.com/wp-content/uploads/2015/08/unnamed.jpg",
+  "scores":[
+     3,
+     4,
+     5,
+     5,
+     2,
+     5,
+     1,
+     3,
+     4,
+     1
+   ]
+}
+
+{
+  "name":"Amelia",
+  "photo":"http://seductionfaq.com/blog/wp-content/uploads/2012/05/loophole-in-female-psychology.jpg",
+  "scores":[
+     5,
+     3,
+     4,
+     3,
+     5,
+     3,
+     2,
+     3,
+     4,
+     1
+   ]
+}
+
+{
+  "name":"Isla",
+  "photo":"http://cdn.images.express.co.uk/img/dynamic/40/590x/06f37victoria-487055.jpg",
+  "scores":[
+     4,
+     1,
+     5,
+     4,
+     4,
+     1,
+     3,
+     5,
+     2,
+     1
+   ]
+}
+
+{
+  "name":"Noah",
+  "photo":"http://attractmen.org/wp-content/uploads/2015/10/attractmen.org-libra-men.jpg",
+  "scores":[
+     3,
+     2,
+     5,
+     5,
+     5,
+     2,
+     4,
+     4,
+     1,
+     1
+   ]
+}
+
+{
+  "name":"Liam",
+  "photo":"http://creativestockphoto.com/wp-content/uploads/2014/10/images-of-man-and-woman-in-love.jpg",
+  "scores":[
+     2,
+     1,
+     5,
+     4,
+     4,
+     5,
+     3,
+     5,
+     2,
+     3
+   ]
+}
+
+{
+  "name":"Mason",
+  "photo":"http://totalcrack.com/wp-content/uploads/2016/06/Becks1_5_3029072b.jpg",
+  "scores":[
+     3,
+     1,
+     5,
+     4,
+     5,
+     5,
+     5,
+     1,
+     2,
+     3
+   ]
+}
+
+{
+  "name":"Jacob",
+  "photo":"http://www.spring.org.uk/images/man1.jpg",
+  "scores":[
+     4,
+     3,
+     4,
+     2,
+     1,
+     5,
+     5,
+     5,
+     3,
+     1
+   ]
+}
+
+{
+  "name":"William",
+  "photo":"http://img.crazyhdsource.com/wp-content/cover/files/A.Gifted.Man.S01.720p.WEB-DL.DD5.1.H.264-CtrlHD.jpg",
+  "scores":[
+     4,
+     5,
+     5,
+     5,
+     4,
+     5,
+     3,
+     4,
+     2,
+     3
+   ]
+}
+
+{
+  "name":"Ethan",
+  "photo":"http://media.istockphoto.com/photos/portrait-of-smiling-young-man-outdoors-picture-id478022628?k=6&m=478022628&s=170667a&w=0&h=18JwG_PEtRlbcJgQFz2Hjyp78xtnSwNaV9BuhJYUDB0=",
+  "scores":[
+     5,
+     1,
+     1,
+     4,
+     1,
+     3,
+     2,
+     5,
+     3,
+     4
+   ]
+}
+
+{
+  "name":"James",
+  "photo":"https://s-media-cache-ak0.pinimg.com/originals/bf/af/e7/bfafe7e9ae6359a33665ed02a0081671.jpg",
+  "scores":[
+     5,
+     1,
+     5,
+     4,
+     5,
+     2,
+     3,
+     3,
+     4,
+     1
+   ]
+}
+
+{
+  "name":"Alexander",
+  "photo":"http://dp88pot7smo43.cloudfront.net/wp-content/uploads/2015/05/bearded-man-e1430923554819.jpg",
+  "scores":[
+     1,
+     1,
+     1,
+     4,
+     1,
+     2,
+     5,
+     5,
+     3,
+     1
+   ]
+}
+
+{
+  "name":"Ezra",
+  "photo":"http://www.history.co.uk/sites/default/files/images/incopy/heavy-stubble-on-a-man1.jpg",
+  "scores":[
+     5,
+     1,
+     5,
+     5,
+     4,
+     3,
+     1,
+     5,
+     4,
+     1
+   ]
+}
+
+{
+  "name":"Declan",
+  "photo":"https://s-media-cache-ak0.pinimg.com/originals/ee/b2/49/eeb249b1a55239aa65a618f2bcd6e059.jpg",
+  "scores":[
+     5,
+     1,
+     3,
+     4,
+     4,
+     5,
+     3,
+     5,
+     3,
+     1
+   ]
+}
