d0d4df6b0232929e44b99e16dcd252e6d45b2e69
illynabruin92 <alvaivette@g.ucla.edu>
2016-09-28 22:03:52 -0700

    updated api/html routes

---
+++ server.js
@@ -0,0 +1,8 @@
+// ==============================================================================
+// DEPENDENCIES
+// Series of npm packages that we will use to give our server useful functionality
+// ==============================================================================
+
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
