28510b0de5b97562719d4dc0f6dcbef0e08b8194
illynabruin92 <alvaivette@g.ucla.edu>
2016-09-29 18:33:27 -0700

    server.js

--- server.js
+++ server.js
@@ -6,3 +6,11 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
+
+//Set up port 5000, heroku default
+var PORT = 5000;
+
+//Starts server
+server.listen(process.env.PORT ||) {
+  console.log("Server is listening on PORT: " + PORT);
+});
