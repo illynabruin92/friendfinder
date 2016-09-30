f7e7ada625ad1d2173d2d511888fb14ff92478e0
illynabruin92 <alvaivette@g.ucla.edu>
2016-09-29 17:58:11 -0700

    difference alg added

--- app/data/friends.js
+++ app/data/friends.js
@@ -5,24 +5,9 @@ Boy names: Noah, Liam, Mason, Jacob, William, Ethan, James, Alexander, Michael, 
 */
 
 
-{
-  "name":"Olivia",
-  "photo":"https://i.ytimg.com/vi/yevSZtUmjes/maxresdefault.jpg",
-  "scores":[
-     5,
-     1,
-     4,
-     4,
-     5,
-     1,
-     2,
-     5,
-     4,
-     1
-   ]
-}
 
-{
+
+var friend1 = {
   "name":"Sophia",
   "photo":"http://femaleloophole.org/wp-content/uploads/2011/07/female-psychology-loophole.jpg",
   "scores":[
@@ -39,7 +24,7 @@ Boy names: Noah, Liam, Mason, Jacob, William, Ethan, James, Alexander, Michael, 
    ]
 }
 
-{
+var friend2 = {
   "name":"Ava",
   "photo":"http://kingofwallpapers.com/female/female-022.jpg",
   "scores":[
@@ -56,7 +41,7 @@ Boy names: Noah, Liam, Mason, Jacob, William, Ethan, James, Alexander, Michael, 
    ]
 }
 
-{
+var friend3 = {
   "name":"Isabella",
   "photo":"http://fashion.blogorganization.com/wp-includes/images/322female-celebrity-long-hair-photos-04.jpg",
   "scores":[
@@ -73,7 +58,7 @@ Boy names: Noah, Liam, Mason, Jacob, William, Ethan, James, Alexander, Michael, 
    ]
 }
 
-{
+var friend4 = {
   "name":"Mia",
   "photo":"https://s-media-cache-ak0.pinimg.com/originals/3d/2f/45/3d2f45f6f3ef6164fcf793aa9cce1968.jpg",
   "scores":[
@@ -90,24 +75,8 @@ Boy names: Noah, Liam, Mason, Jacob, William, Ethan, James, Alexander, Michael, 
    ]
 }
 
-{
-  "name":"Abigail",
-  "photo":"https://i.ytimg.com/vi/L9ybIqXeRNA/hqdefault.jpg",
-  "scores":[
-     5,
-     1,
-     4,
-     4,
-     5,
-     1,
-     2,
-     5,
-     4,
-     1
-   ]
-}
 
-{
+var friend5 = {
   "name":"Charlotte",
   "photo":"https://assets.entrepreneur.com/content/16x9/822/1387219460-leadership-lessons-young-female-entrepreneurs-andi-atteberry-blingsting.jpg",
   "scores":[
@@ -124,7 +93,7 @@ Boy names: Noah, Liam, Mason, Jacob, William, Ethan, James, Alexander, Michael, 
    ]
 }
 
-{
+var friend6 = {
   "name":"Harper",
   "photo":"https://sosv.com/wp-content/uploads/2015/08/unnamed.jpg",
   "scores":[
@@ -141,7 +110,7 @@ Boy names: Noah, Liam, Mason, Jacob, William, Ethan, James, Alexander, Michael, 
    ]
 }
 
-{
+var friend7 = {
   "name":"Amelia",
   "photo":"http://seductionfaq.com/blog/wp-content/uploads/2012/05/loophole-in-female-psychology.jpg",
   "scores":[
@@ -158,7 +127,7 @@ Boy names: Noah, Liam, Mason, Jacob, William, Ethan, James, Alexander, Michael, 
    ]
 }
 
-{
+var friend8 = {
   "name":"Isla",
   "photo":"http://cdn.images.express.co.uk/img/dynamic/40/590x/06f37victoria-487055.jpg",
   "scores":[
@@ -175,7 +144,7 @@ Boy names: Noah, Liam, Mason, Jacob, William, Ethan, James, Alexander, Michael, 
    ]
 }
 
-{
+var friend9 = {
   "name":"Noah",
   "photo":"http://attractmen.org/wp-content/uploads/2015/10/attractmen.org-libra-men.jpg",
   "scores":[
@@ -192,7 +161,7 @@ Boy names: Noah, Liam, Mason, Jacob, William, Ethan, James, Alexander, Michael, 
    ]
 }
 
-{
+var friend10 = {
   "name":"Liam",
   "photo":"http://creativestockphoto.com/wp-content/uploads/2014/10/images-of-man-and-woman-in-love.jpg",
   "scores":[
@@ -209,7 +178,7 @@ Boy names: Noah, Liam, Mason, Jacob, William, Ethan, James, Alexander, Michael, 
    ]
 }
 
-{
+var friend11 = {
   "name":"Mason",
   "photo":"http://totalcrack.com/wp-content/uploads/2016/06/Becks1_5_3029072b.jpg",
   "scores":[
@@ -226,7 +195,7 @@ Boy names: Noah, Liam, Mason, Jacob, William, Ethan, James, Alexander, Michael, 
    ]
 }
 
-{
+var friend12 = {
   "name":"Jacob",
   "photo":"http://www.spring.org.uk/images/man1.jpg",
   "scores":[
@@ -243,7 +212,7 @@ Boy names: Noah, Liam, Mason, Jacob, William, Ethan, James, Alexander, Michael, 
    ]
 }
 
-{
+var friend13 = {
   "name":"William",
   "photo":"http://img.crazyhdsource.com/wp-content/cover/files/A.Gifted.Man.S01.720p.WEB-DL.DD5.1.H.264-CtrlHD.jpg",
   "scores":[
@@ -260,7 +229,7 @@ Boy names: Noah, Liam, Mason, Jacob, William, Ethan, James, Alexander, Michael, 
    ]
 }
 
-{
+var friend14 = {
   "name":"Ethan",
   "photo":"http://media.istockphoto.com/photos/portrait-of-smiling-young-man-outdoors-picture-id478022628?k=6&m=478022628&s=170667a&w=0&h=18JwG_PEtRlbcJgQFz2Hjyp78xtnSwNaV9BuhJYUDB0=",
   "scores":[
@@ -277,7 +246,7 @@ Boy names: Noah, Liam, Mason, Jacob, William, Ethan, James, Alexander, Michael, 
    ]
 }
 
-{
+var friend15 = {
   "name":"James",
   "photo":"https://s-media-cache-ak0.pinimg.com/originals/bf/af/e7/bfafe7e9ae6359a33665ed02a0081671.jpg",
   "scores":[
@@ -294,7 +263,7 @@ Boy names: Noah, Liam, Mason, Jacob, William, Ethan, James, Alexander, Michael, 
    ]
 }
 
-{
+var friend16 = {
   "name":"Alexander",
   "photo":"http://dp88pot7smo43.cloudfront.net/wp-content/uploads/2015/05/bearded-man-e1430923554819.jpg",
   "scores":[
@@ -311,7 +280,7 @@ Boy names: Noah, Liam, Mason, Jacob, William, Ethan, James, Alexander, Michael, 
    ]
 }
 
-{
+var friend17 = {
   "name":"Ezra",
   "photo":"http://www.history.co.uk/sites/default/files/images/incopy/heavy-stubble-on-a-man1.jpg",
   "scores":[
@@ -328,7 +297,7 @@ Boy names: Noah, Liam, Mason, Jacob, William, Ethan, James, Alexander, Michael, 
    ]
 }
 
-{
+var friend18 = {
   "name":"Declan",
   "photo":"https://s-media-cache-ak0.pinimg.com/originals/ee/b2/49/eeb249b1a55239aa65a618f2bcd6e059.jpg",
   "scores":[
